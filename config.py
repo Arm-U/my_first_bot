f37128f862dc0c22d52a4753295e5add5daa2b28
Arman Urazgulov <Urazgulov.A@phystech.edu>
2020-05-25 06:56:42 +0300

    added mipt, api id, hash id

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 CONFIG = {
     'TOKEN': 'HereShouldBeToken',
     'link': {
-        'RM': 'https://realmadrid.ru/'
+        'RM': 'https://realmadrid.ru/',
+        'MIPT': 'https://mipt.ru/'
     },
     'headers': {
         'user-agent':
@@ -9,5 +10,7 @@ CONFIG = {
             '(Windows NT 10.0; Win64; x64) '
             'AppleWebKit/537.36 '
             '(KHTML, like Gecko) '
-            'Chrome/81.0.4044.138 Safari/537.36'}
+            'Chrome/81.0.4044.138 Safari/537.36'},
+    'api_id': 'HereShouldBeApiId',
+    'api_hash': 'HereShouldBeApiHash'
 }
